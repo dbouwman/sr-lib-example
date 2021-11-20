d9b6f8e5ad23ce46ab0ba306e1adf10ae691580f
Dave Bouwman <dave@davebouwman.com>
2021-11-20 09:58:04 -0700

    feat: subtract fn

--- test/sum.test.ts
+++ test/sum.test.ts
@@ -1,7 +1,13 @@
-import { sum } from '../src';
+import { subtract, sum } from '../src';
 
-describe('blah', () => {
+describe('sum', () => {
   it('works', () => {
     expect(sum(1, 1, 2)).toEqual(4);
+  });
+});
+
+describe('subtract', () => {
+  it('works', () => {
+    expect(subtract(5, 2)).toEqual(3);
   });
 });
